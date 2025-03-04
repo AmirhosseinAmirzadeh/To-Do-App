89017b86de4f6a15784761a42a4dd67003b37dd6
Amir Hossein <amirzadeh@live.com>
2025-03-04 16:39:03 +0330

    CustomUserModel

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    """ custom admin panel for user management with add and change forms plus password """
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active', 'is_verified')
+    list_filter = ('email', 'is_superuser', 'is_active', 'is_verified')
+    search_fields = ('email',)
+    ordering = ('email',)
+    # fieldsets are for current users lists
+    fieldsets = (
+        (
+            'Authentication',
+            {
+                'fields': ('email', 'password'),
+            },
+        ),
+        (
+            'permissions',
+            {
+                'fields': ('is_staff', 'is_active', 'is_superuser', 'is_verified'),
+            },
+        ),
+    )
+
+    # group permissions
+    (
+        'group permissions',
+        {'fields': ('group', 'user_permissions')},
+    )
+
+    (
+        'important date',
+        {'fields': ('last_login',)},
+    )
+
+    # add_fieldsets are for making new users
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'is_staff',
+                    'is_active',
+                    'is_superuser',
+                    'is_verified',
+                ),
+            },
+        ),
+    )
+    
+admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0346e-kzkiimardwp(c7h*=rfynoh5aftahlqmp&fz$vv2a$)!'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-0346e-kzkiimardwp(c7h*=rfynoh5aftahlqmp&fz$vv2a$)!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    cast=lambda v: [s.strip() for s in v.split(',')],
+    default='*',
+)
 
 
 # Application definition
@@ -122,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User manager config
+AUTH_USER_MODEL = 'accounts.User'
