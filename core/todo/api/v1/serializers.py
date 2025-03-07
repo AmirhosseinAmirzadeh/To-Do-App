e3a52712f670f31b0e164169cb960233f6af9da8
Amir Hossein <amirzadeh@live.com>
2025-03-07 15:16:23 +0330

    Update serializers.py
    I just added id field to have better control over tasks.

--- core/todo/api/v1/serializers.py
+++ core/todo/api/v1/serializers.py
@@ -14,7 +14,7 @@ class TodoSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Todo
-        fields = ['user', 'title', 'due_time', 'complete', 'created_date', 'updated_date']
+        fields = ['user', 'id', 'title', 'due_time', 'complete', 'created_date', 'updated_date']
         read_only_fields = ['user']
         
     def to_representation(self, instance):
